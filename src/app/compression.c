0d8c399aa584ac9a2d0e9679c3c25652044b7645
Mayank <mayanktiwari1212.mt@gmail.com>
2025-01-04 17:51:09 +0530

    feat: Added huffman code generation

--- src/app/compression.c
+++ src/app/compression.c
@@ -28,8 +28,7 @@ void write_bit(FILE *output, BitWriter *writer, int bit)
     }
 }
 
-
-void flush_bits(FILE* output, BitWriter* writer)
+void flush_bits(FILE *output, BitWriter *writer)
 {
     if (writer->position > 0)
     {
@@ -38,18 +37,21 @@ void flush_bits(FILE* output, BitWriter* writer)
     }
 }
 
-int compress_file(const char* filename) {
+int compress_file(const char *filename)
+{
     FILE *input = fopen(filename, "rb");
-    if (!input) {
+    if (!input)
+    {
         printf("Error: Could not open file %s\n", filename);
         return 1;
     }
 
-    char* output_filename = malloc(strlen(filename) + 5);
+    char *output_filename = malloc(strlen(filename) + 5);
     sprintf(output_filename, "%s.huf", filename);
-    
+
     FILE *output = fopen(output_filename, "wb");
-    if (!output) {
+    if (!output)
+    {
         printf("Error: Could not create file %s\n", output_filename);
         fclose(input);
         free(output_filename);
@@ -60,8 +62,9 @@ int compress_file(const char* filename) {
     size_t file_size = ftell(input);
     fseek(input, 0, SEEK_SET);
 
-    unsigned char* buffer = malloc(file_size);
-    if (!buffer) {
+    unsigned char *buffer = malloc(file_size);
+    if (!buffer)
+    {
         printf("Error: Could not allocate memory for file buffer\n");
         fclose(input);
         fclose(output);
@@ -70,8 +73,9 @@ int compress_file(const char* filename) {
     }
 
     int unique_chars;
-    CharFreq* frequency_table = count_frequencies(buffer, file_size, &unique_chars);
-    if (!frequency_table) {
+    CharFreq *freq_table = count_frequencies(buffer, file_size, &unique_chars);
+    if (!freq_table)
+    {
         printf("Error: Could not count character frequencies\n");
         fclose(input);
         fclose(output);
@@ -80,6 +84,23 @@ int compress_file(const char* filename) {
         return 1;
     }
 
+    // HUFFMAN TREE BUILDING
+    HuffmanNode *root = build_huffman_tree(freq_table, unique_chars);
+    if (!root)
+    {
+        printf("Error: Could not build Huffman tree\n");
+        fclose(input);
+        fclose(output);
+        free(output_filename);
+        free(buffer);
+        free(freq_table);
+        return 1;
+    }
+
+    // HUFFMAN CODE GENERATION
+    HuffmanCode codes[256] = {0};
+    char temp_code[256] = {0};
+    generate_huffman_codes(root, codes, temp_code, 0);
 
     
 }
